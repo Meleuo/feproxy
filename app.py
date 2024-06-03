9c463f5568a5e8c9f87640001951e1c9255efe29
Your Name <you@example.com>
2024-06-03 17:37:13 +0800

    Mon Jun  3 05:37:13 PM CST 2024

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from urllib.parse import quote
 
 size_limit = 1024 * 1024 * 1024 * 999  # 允许的文件大小，默认999GB，相当于无限制了 https://github.com/hunshcn/gh-proxy/issues/8
 
-HOST = '127.0.0.1'  # 监听地址，建议监听本地然后由web服务器反代
+HOST = '0.0.0.0'  # 监听地址，建议监听本地然后由web服务器反代
 PORT =  50200  # 监听端口
 
 black_list = []
@@ -72,18 +72,27 @@ def iter_content(self, chunk_size=1, decode_unicode=False):
 
 
 
-@app.route('/<path:u>', methods=['GET', 'POST'])
+#@app.route('/<path:u>', methods=['GET', 'POST'])
+@app.route('/<path:u>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'])
 def handler(u):
+    print('Url-->',u)
     u = u if u.startswith('http') else 'https://' + u
     if u.rfind('://', 3, 9) == -1:
         u = u.replace('s:/', 's://', 1)  # uwsgi会将//传递为/
+    u = u.replace('/info/refs?service=git-upload-pack', '')  # uwsgi会将//传递为/a
+    u = u.replace('/info/refs', '')  # uwsgi会将//传递为/a
+    print('Url-->',u)
+    # 打印全部参数
+    
+    
+    
+    print(request.data)
+    print(request.args)
+
     for i in black_list:
         if i in u:
             return Response('Invalid input.', status=403)
-    return proxy(u)
 
-
-def proxy(u, allow_redirects=False):
     headers = {}
     r_headers = dict(request.headers)
     if 'Host' in r_headers:
@@ -92,22 +101,15 @@ def proxy(u, allow_redirects=False):
         url = u + request.url.replace(request.base_url, '', 1)
         if url.startswith('https:/') and not url.startswith('https://'):
             url = 'https://' + url[7:]
-        r = requests.request(method=request.method, url=url, data=request.data, headers=r_headers, stream=True, allow_redirects=allow_redirects)
+        r = requests.request(method=request.method, url=url, data=request.data, headers=r_headers, stream=True, allow_redirects=True)
         headers = dict(r.headers)
-
+    
         if 'Content-length' in r.headers and int(r.headers['Content-length']) > size_limit:
             return redirect(u + request.url.replace(request.base_url, '', 1))
 
         def generate():
             for chunk in iter_content(r, chunk_size=CHUNK_SIZE):
                 yield chunk
-
-        if 'Location' in r.headers:
-            _location = r.headers.get('Location')
-            if _location:
-                headers['Location'] = '/' + _location
-            else:
-                return proxy(_location, True)
 
         return Response(generate(), headers=headers, status=r.status_code)
     except Exception as e:
